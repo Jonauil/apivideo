d4e6a13a23d94b2ddbe6cc21c443a4fbf4c04757
jonauil <huangzai@dingtalk.com>
2018-02-13 17:05:33 +0800

    GetALL method

--- models/object.go
+++ models/object.go
@@ -8,18 +8,8 @@ import (
 )
 
 var (
-	Objects map[string]*Object
-	Videos  map[string]*Info
-	num int64
-	//Videoslist map[string] []*Info
-	VideosMaps []orm.Params
+	Videoslist []*Info
 )
-
-type Object struct {
-	ObjectId   string
-	Score      int64
-	PlayerName string
-}
 
 type Info struct {
 	Id int            `orm:"column(id);auto"`
@@ -29,13 +19,6 @@ type Info struct {
 	CreateDate time.Time  `orm:"column(createdate);type(datetime)"`
 }
 
-func init() {
-	Objects = make(map[string]*Object)
-	Videos = make(map[string]*Info)
-	//Videoslist := make(map[string] []*Info)
-	Objects["hjkhsbnmn123"] = &Object{"hjkhsbnmn123", 100, "astaxie"}
-	Objects["mjjkxsxsaa23"] = &Object{"mjjkxsxsaa23", 101, "someone"}
-}
 
 func AddOne(info *Info) (id int64,err error) {
 	o := orm.NewOrm()
@@ -55,36 +38,13 @@ func GetOne(id int) (info *Info,err error) {
 
 }
 
-func GetAll() (map[string] interface{}) {
-	//info := Info{}
-	/*
-	fmt.Println("info=",info)
+func GetAll() ([]*Info){
 	orm.Debug = true
 	o := orm.NewOrm()
-	err := o.Read(&info)
-	if err == orm.ErrNoRows {
-		fmt.Println("查询不到")
-	}else if err == orm.ErrMissPK{
-		fmt.Println("查询不到主键")
-	}else {
-		fmt.Printf("Id %v \n title %v ImageHref %v Href %v CreateDate %v",info.Id,info.Title,info.ImageHref,info.Href,info.CreateDate)
-		Videos["videllist"] = &info
-	}*/
-	o := orm.NewOrm()
-    videoslist := make(map[string] interface{})
-   // VideoData := make(map[string] interface{})
-	num,err := o.Raw("SELECT * FROM info").Values(&VideosMaps)
-	fmt.Println("num:",num)
-	for k,v := range VideosMaps{
-		fmt.Println("key:",k,"value:",v)
-		videoslist["videodata"] = v
+	q := o.QueryTable("info")
+	q.All(&Videoslist)
+	return Videoslist
 
-	}
-	if err != nil {
-		fmt.Println("查询不到")
-	}
-	//Videos["videolist"] = &info
-	return videoslist
 }
 
 func Update(info *Info) (err error){
